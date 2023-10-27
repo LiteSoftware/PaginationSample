c6c03c3f7b9fccbba79d86308aa84ed2eb07d047
javavirys <javavirys@gmail.com>
2023-10-27 11:37:01 +0300

    add copyright, remove tests

--- app/src/main/java/com/udfsoft/pagination/MainActivity.kt
+++ app/src/main/java/com/udfsoft/pagination/MainActivity.kt
@@ -1,15 +1,23 @@
+/*
+ *    Copyright 2023 javavirys
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 package com.udfsoft.pagination
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.lifecycleScope
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
@@ -17,60 +25,31 @@ import androidx.paging.PagingData
 import androidx.recyclerview.widget.RecyclerView
 import com.udfsoft.pagination.core.entity.MyItem
 import com.udfsoft.pagination.data.MyDataSource
-import com.udfsoft.pagination.data.MyDataSourceFactory
 import com.udfsoft.pagination.data.MyRepository
 import com.udfsoft.pagination.ui.MyPagingDataAdapter
-import com.udfsoft.pagination.ui.theme.PaginationSampleTheme
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContent {
-//            PaginationSampleTheme {
-//                // A surface container using the 'background' color from the theme
-//                Surface(
-//                    modifier = Modifier.fillMaxSize(),
-//                    color = MaterialTheme.colorScheme.background
-//                ) {
-//                    Greeting("Android")
-//                }
-//            }
-//        }
+
         val pagingConfig = PagingConfig(
             pageSize = 20,
             enablePlaceholders = false
         )
 
-        val repository = MyRepository() // Замените на ваш репозиторий
-        val dataSourceFactory = MyDataSource(repository)//MyDataSourceFactory(repository)
-        val pager = Pager(pagingConfig) { dataSourceFactory }.flow
-        val pagingData: Flow<PagingData<MyItem>> = pager
+        val repository = MyRepository()
+        val dataSourceFactory = MyDataSource(repository)
+        val pager = Pager(pagingConfig) { dataSourceFactory }
+        val pagingData: Flow<PagingData<MyItem>> = pager.flow
         val adapter = MyPagingDataAdapter()
         findViewById<RecyclerView>(R.id.recyclerView).adapter = adapter
+
         lifecycleScope.launch {
             pagingData.collect {
                 adapter.submitData(it)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PaginationSampleTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/udfsoft/pagination/data/MyRepository.kt
+++ app/src/main/java/com/udfsoft/pagination/data/MyRepository.kt
@@ -1,3 +1,19 @@
+/*
+ *    Copyright 2023 javavirys
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 package com.udfsoft.pagination.data
 
 import com.udfsoft.pagination.core.entity.MyItem
@@ -13,7 +29,7 @@ class MyRepository {
         private val DATA = generateData().chunked(20)
 
         private fun generateData() = mutableListOf<MyItem>().apply {
-            (0..123).forEach {
+            (0..10_000).forEach {
                 add(MyItem(it, it.toString()))
             }
         }
